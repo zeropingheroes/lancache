c63bf13622b68b6cf1f59897bcde741b885c9ebf
ilumos <ilumos@gmail.com>
2017-01-16 17:21:40 +0000

    Changed location of steam server lists storage

--- install/prepare-directories.sh
+++ install/prepare-directories.sh
@@ -9,7 +9,7 @@ WWW_USER="www-data"
 echo "Creating cache data directory: $PATH_CACHE"
 mkdir -p $PATH_CACHE
 cd $PATH_CACHE
-mkdir -p installs other tmp
+mkdir -p installs other tmp steam-servers
 
 echo "Creating logs directory: $PATH_LOGS"
 mkdir -p $PATH_LOGS
