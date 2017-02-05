c0c420320bcf7ca3fe7d76ea44d68e48c8ac9c18
ilumos <ilumos@gmail.com>
2017-02-05 01:13:43 +0000

    Adding range support to refresh-cached-url script

--- scripts/refresh-cached-url.sh
+++ scripts/refresh-cached-url.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-echo "Enter in URL (plus query string) that should be refreshed from upstream:"
+if [[ $# -eq 0 ]] ; then
+    echo ''
+    echo 'Usage: refresh-cached-url.sh [url] [range]'
+    echo ''
+    echo 'Example:'
+    echo ''
+    echo 'refresh-cached-url.sh "http://origin-a.akamaihd.net/eamaster/s/shift/b____553.zip?sauth=112321235_13bc59b3ec0ffdfdabc9d53e583f6c9" "bytes=5306843136-5307891711"'
+    echo ''
+    exit 0
+fi
 
-read URL
-
-curl --head "$URL&nocache=1"
+if [[ -n "$2" ]]
+then
+	curl --header "Range: $2" --head "$1&nocache=1"
+else
+	curl --head "$1&nocache=1"
+fi
